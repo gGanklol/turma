1c5cc18beefa56a598d5b186e79c95855aa32512
gGanklol <sev7n78.76@gmail.com>
2023-03-30 17:16:07 -0300

    first commit

---
+++ algoritmos legais/idadeviaget.php
@@ -0,0 +1,11 @@
+<?php
+//  exemplo usando o metodo POST.
+$nome = $_GET["nome"];
+$email = $_GET["email"];
+$idade = $_GET["idade"];
+
+echo "Bem Vindo ao mundo PHP $nome <br>";
+echo "E-mail registrado $email";
+echo "Idade Registrado :) $idade";
+
+?>
---
+++ algoritmos legais/idadeviapost.php
@@ -0,0 +1,11 @@
+<?php
+//  exemplo usando o metodo POST.
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$idade = $_POST["idade"];
+
+echo "Bem Vindo ao mundo PHP $nome <br>";
+echo "E-mail registrado $email";
+echo "Idade Registrada ;) $idade";
+
+?>
